8a28db283f933d8a74b5fb15e20dc0b395b53010
hehysh <hehysh@163.com>
2024-05-05 23:01:41 +0800

    workflow[bot]

---
+++ assets/FrameBlank.5471ce42.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.4eef60d5.js";import{B as a,D as r,F as n}from"./vendor.a33f6cba.js";const o=a({name:"FrameBlank"});function t(s,c,m,p,_,f){return r(),n("div")}var l=e(o,[["render",t]]);export{l as default};
